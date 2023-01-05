96f3a711887c7b0addf67035ff4fff0deb8b9a50
trishamishra2700 <121280381+trishamishra2700@users.noreply.github.com>
2023-01-05 09:44:37 -0800

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,81 @@
+let MCQS = [
+            {
+            question: "Which one of the following is the process of inserting an element in the stack?",
+    choice1: "Insert",
+    choice2: "Add",
+    choice3: "Push",
+    choice4: "None of the above",
+    answer: 2
+},
+            {
+            question: " ___________is the technology that connects the machines and people within a site in a small area.",
+    choice1: "LAN",
+    choice2: "MAN",
+    choice3: "WAN",
+    choice4: "None of these",
+    answer: 0
+},
+            {
+            question: "Which of the following is not an example of DBMS?",
+    choice1: "MySQL",
+    choice2: "Microsoft Acess",
+    choice3: "IBM DB2",
+    choice4: "Google",
+    answer: 3
+    
+},
+            {
+            question: "The part of the computer that anyone can touch.",
+    choice1: "Hardware",
+    choice2: "Software",
+    choice3: "Data",
+    choice4: "Input",
+    answer: 0
+},
+            {
+            question: "Who is the father of PHP language?",
+    choice1: "Dennis Ritchie",
+    choice2: "James Gosling",
+    choice3: "Rasmus Lerdorf",
+    choice4: "Charles Luce",
+    answer: 2
+},
+            {
+            question: "What type of CSS is generally recommended for designing large web pages?",
+    choice1: "Inline",
+    choice2: "Internal",
+    choice3: "External",
+    choice4: "None of the above",
+    answer: 2
+},
+            {
+               question: "Which of the following is not a valid C variable name?",
+    choice1: "int number;",
+    choice2: "float rate;",
+    choice3: "int variable_count;",
+    choice4: "int $main;",
+    answer: 3
+},{
+               question: "All keywords in C are in ___  ",
+    choice1: "LowerCase letters",
+    choice2: "UpperCase letters",
+    choice3: "CamelCase letters",
+    choice4: "None of the mentioned",
+    answer: 0
+},
+            {
+               question: "If the Area of Circle is 154 cm2 , then its perimeter  is",
+    choice1: "11 cm",
+    choice2: "44 cm",
+    choice3: "22 cm",
+    choice4: "55 cm",
+    answer: 1
+},
+            {
+               question: "Which of the following methods is used to access HTML element in javascript?",
+    choice1: "getElementById()",
+    choice2: "getElementByClassName()",
+    choice3: "Both A and B",
+    choice4: "None of the above",
+    answer: 2
+}];
